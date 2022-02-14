74adf07e33946453f79f8da1c00a696b0fc31b69
balancy <balancy@gmail.com>
2022-02-14 12:12:28 +0100

    Write chat messages to file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import asyncio
+from datetime import datetime
+
+import aiofiles
 
 HOST = ('minechat.dvmn.org', 5000)
 
@@ -6,9 +9,14 @@ HOST = ('minechat.dvmn.org', 5000)
 async def listen_chat():
     reader, writer = await asyncio.open_connection(*HOST)
 
-    while not reader.at_eof():
-        data = await reader.readline()
-        print(data.decode().rstrip())
+    async with aiofiles.open('chat_history.txt', 'a') as file:
+        while not reader.at_eof():
+            data = await reader.readline()
+            current_time = datetime.now().strftime('[%m.%d.%Y %H:%M]')
+            chat_message = data.decode()
+            logged_message = f'{current_time} {chat_message}'
+            await file.write(logged_message)
+            print(logged_message.rstrip())
 
     writer.close()
 
