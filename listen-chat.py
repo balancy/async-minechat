4445b81c049d4346e80a03738f9b22d75e41a23f
balancy <balancy@gmail.com>
2022-02-14 15:56:42 +0100

    Add CLI options

---
+++ listen-chat.py
@@ -0,0 +1,51 @@
+import argparse
+import asyncio
+from datetime import datetime
+
+import aiofiles
+from environs import Env
+
+async def listen_chat(host: str, port: int, history_file: str):
+    reader, writer = await asyncio.open_connection(host, port)
+
+    async with aiofiles.open(history_file, 'a') as file:
+        while not reader.at_eof():
+            chat_message = await reader.readline()
+
+            current_time = datetime.now().strftime('[%m.%d.%Y %H:%M]')
+            logged_message = f'{current_time} {chat_message.decode()}'
+
+            await file.write(logged_message)
+            print(logged_message.rstrip())
+
+    writer.close()
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+
+    parser = argparse.ArgumentParser(
+        description='Define optional arguments like host, port, history file.'
+    )
+    parser.add_argument(
+        '--host',
+        type=str,
+        default=env.str('HOST', 'minechat.dvmn.org'),
+        help='Chat host to listen to',
+    )
+    parser.add_argument(
+        '--port',
+        type=int,
+        default=env.int('PORT', 5000),
+        help='Chat port',
+    )
+    parser.add_argument(
+        '--history',
+        type=str,
+        default=env.str('HISTORY', 'minechat.history'),
+        help='The file to save chat history to',
+    )
+    args = parser.parse_args()
+
+    asyncio.run(listen_chat(args.host, args.port, args.history))
